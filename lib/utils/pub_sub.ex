ebe98192209dbfc2c9ebb7df45ededdc6e90c0ab
Clay Sampson <pdgeek@gmail.com>
2021-04-02 08:26:55 -0400

    wip

--- lib/utils/pub_sub.ex
+++ lib/utils/pub_sub.ex
@@ -3,7 +3,7 @@ defmodule PubSub do
   PubSub module contains modules and functions related to pub/sub. Duh.
   """
 
-  # use Utils.PubSubHelpers
+  use Utils.PubSubHelpers
 
   # Notifications about new sensors, source is a radio source, type is in ["honeywell_345"]
   # define_channel("sensor_discovery", source: nil, type: nil)
